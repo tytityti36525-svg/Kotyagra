2c954218749b7450ccddbdb360a42000b6941fca
tytityti36525-svg <tytityti36525@gmail.com>
2026-06-08 14:40:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCVlCIXLQ_GMBPMR8A5-8myzkuolm-BCs",
+    authDomain: "kotyagra-d737c.firebaseapp.com",
+    projectId: "kotyagra-d737c",
+    storageBucket: "kotyagra-d737c.firebasestorage.app",
+    messagingSenderId: "674427128723",
+    appId: "1:674427128723:web:a4ee00e479a0b284fb3666",
+    measurementId: "G-GVHYLFV3P9"
+  };
